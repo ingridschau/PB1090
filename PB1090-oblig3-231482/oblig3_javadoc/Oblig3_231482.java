f8830e142a271bb8bcea1c25c7e50fab195b97dd
Ingrid Schau <ingrid.te.schau@gmail.com>
2022-03-20 15:03:54 +0100

    Changes

---
+++ PB1090-oblig3-231482/oblig3_javadoc/Oblig3_231482.java
@@ -0,0 +1,15 @@
+import java.io.File; 
+import java.io.FileNotFoundException;  
+import java.util.*;
+
+public class Oblig3_231482 {
+
+    public static  void main(String[] args) {
+
+        Matrise matrise = new Matrise("testdata.txt");
+
+        System.out.println("\nMaks summen for en rad er: " + matrise.maksSum + " paa rad: " + matrise.maksSumRadNr);
+
+        
+    }
+}
