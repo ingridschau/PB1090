4f1198e38a09b4236d23ec4a1511a5f096fabad1
Ingrid Schau <ingrid.te.schau@gmail.com>
2022-04-21 12:16:41 +0200

    More exercises

---
+++ compendium/parallel-programming/Oppgave19-1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+// Sequential implementation of QuickSort -- very fast for few elements (<500)
+class Oppgave19-1 {
+    void sQuick(int[] a) {
+        sQuick(a, 0, a.length-1);
+    }
+
+    void
+}
