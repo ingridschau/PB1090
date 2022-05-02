7e79b7526b72c9a19ef111391d9dc9d009c9b38d
Ingrid Schau <ingrid.te.schau@gmail.com>
2022-05-02 10:13:27 +0200

    Added exercises for chapter 18

--- introduction-to-java/chapter-18/exercise-18.20/DisplayCircles.java
+++ introduction-to-java/chapter-18/exercise-18.20/DisplayCircles.java
@@ -8,8 +8,9 @@ import javafx.scene.paint.Color;
 
 /*
 (Display circles) Write a Java program that displays ovals, as shown in
-Figure 18.12b. The circles are centered in the pane. The gap between two adjacent circles is 10 pixels, and the gap between the border of the pane and the
-largest circle is also 10.
+Figure 18.12b. The circles are centered in the pane. The gap between two 
+adjacent circles is 10 pixels, and the gap between the border of the pane 
+and the largest circle is also 10.
 */
 
 public class DisplayCircles extends Application {
